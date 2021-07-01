26638815399505a0677832bf758f07161bcc3301
fizioh <batrom@laposte.net>
2021-07-01 10:12:21 +0200

    basedir pathh

--- retro_project/settings.py
+++ retro_project/settings.py
@@ -145,10 +145,10 @@ import dj_database_url
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
